b76c32e54ba9b11d6a0e133a44acdcfa3edfa231
kmansfield27 <36116244+kmansfield27@users.noreply.github.com>
2020-07-22 23:00:27 -0400

    Add event handlers for job role and design select

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // Global variables
     const name = document.getElementById('name');
     const jobSelect = document.getElementById('title');
+    const designSelect = document.getElementById('design');
+    const designOptions = document.querySelectorAll('#design option');
 
+    designOptions[0].hidden = true;
 
     // Set name input to focus by default
     const focusOnLoad = (inputName) => {
@@ -19,16 +22,54 @@ document.addEventListener('DOMContentLoaded', function() {
             jobOther.style.display = 'none';
         }
     }
+    
+   // Handler for Design select
+   const showShirtColor = () => {
+        const colorOptions = document.getElementById('color').options;
+        const punShirtColors = ['cornflowerblue', 'darkslategrey', 'gold'];
+        const heartShirtColors = ['tomato', 'steelblue', 'dimgrey'];
+        const punOptions = [];
+        const heartOptions = [];
+
+        for (let i = 0; i < colorOptions.length; i++) {
+            colorOptions[i].hidden = true;
+            const colorOptionValue = colorOptions[i].value;
+            const isPunShirtColor = punShirtColors.includes(colorOptionValue);
+            const isHeartShirtColor = heartShirtColors.includes(colorOptionValue);
+
+            if (isPunShirtColor) {
+                punOptions.push(colorOptions[i]);
+            } else if (isHeartShirtColor) {
+                heartOptions.push(colorOptions[i]);
+            }
+        }
+
+        if (event.target.value === 'js puns') {
+            for ( let i = 0; i < punOptions.length; i++ ) {
+                punOptions[i].hidden = false;
+            }
+            punOptions[0].selected = true;
+        } else if (event.target.value === 'heart js') {
+            for ( let i = 0; i < heartOptions.length; i++ ) {
+                heartOptions[i].hidden = false;
+            }
+            heartOptions[0].selected = true;
+        }
+    }
+
+
+    // Initialize functions
+    focusOnLoad(name);
+    showOtherJob();
 
 
     // Callback functions
     jobSelect.addEventListener( 'change', () => {
         showOtherJob();     
     });
-    
 
-    // Initialize functions
-    focusOnLoad(name);
-    showOtherJob();
+    designSelect.addEventListener( 'change', () => {
+        showShirtColor();
+    });
 
 });
