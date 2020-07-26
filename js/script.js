5df1dddc96548a8b54ca2cb9f03ca42c8a1942b0
kmansfield27 <36116244+kmansfield27@users.noreply.github.com>
2020-07-26 01:43:59 -0400

    Refactoring and comments

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    // Global variables
+
+    /*****************************************************/
+    // Global Variables
+    /*****************************************************/
+
     const name = document.getElementById('name');
     const email = document.getElementById('mail');
     const jobSelect = document.getElementById('title');
+    const jobOther = document.getElementById('title-other-container');
     const designSelect = document.getElementById('design');
     const designOptions = document.querySelectorAll('#design option');
     const activities = document.querySelector('.activities');
@@ -20,74 +25,55 @@ document.addEventListener('DOMContentLoaded', function() {
     const totalCostText = document.createElement('p');
 
 
-    // Hide Select Theme option from showing when design select is clicked
-    designOptions[0].hidden = true;
-    activities.appendChild(totalCostText);
-    totalCostText.style.display = 'none';
+    // Create and bind error messages
+    const nameError = createError('span', {className: 'error-message is-hidden', textContent: 'Please enter your name.'});
+    const emailError = createError('span', {className: 'error-message is-hidden', textContent: 'Please enter a valid email.'});
+    const activitiesError = createError('span', {className: 'error-message is-hidden', textContent: 'Please select at least one activity.'});
+    const ccNumError = createError('span', {className: 'error-message is-hidden', textContent: 'Please enter a valid credit card.'});
+    const zipError = createError('span', {className: 'error-message is-hidden', textContent: 'Please enter zip code.'});
+    const cvvError = createError('span', {className: 'error-message is-hidden', textContent: 'Please enter CVV number.'});
+
+
     
     /*****************************************************/
-    // Startup Functions
+    // Startup Function
     /*****************************************************/
 
-    // Set name input to focus by default
-    const inputDefaults = (focusedInput, defaultPayment) => {
+
+    // Default property values, settings, and element insertion needed when initializing
+    const init = () => {
         
-        // Payment method info
-        const paymentOptions = defaultPayment.options;
+        // Set credit card info as the default payment option. Hide content areas for other options.
+        const paymentOptions = payment.options;
         paymentOptions[0].disabled = true;
         paymentOptions[1].selected = true;
         paypal.style.display = 'none';
         bitcoin.style.display = 'none';
 
-        // Min and max of payment fields
+        // Hide the 'Your Job Role' input by default
+        jobOther.style.display = 'none';
+
+        // Set the min and max of the payment fields
         zip.maxLength = 5;
         cvv.maxLength = 3;
         ccNum.maxLength = 16;
 
+        // Hide Select Theme option from showing when design select is clicked
+        designOptions[0].hidden = true;
+        activities.appendChild(totalCostText);
+        totalCostText.style.display = 'none';
 
-        // Focused input
-        focusedInput.focus();
+        // Set name as the default focused input
+        name.focus();
+
+        // Insert default error messages into DOM
+        insertError( name, nameError, 'afterend');   
+        insertError( email, emailError, 'afterend');
+        insertError( activities, activitiesError, 'beforeend');
+        insertError( ccNum, ccNumError, 'afterend');
+        insertError( zip, zipError, 'afterend');
+        insertError( cvv, cvvError, 'afterend');
     }
-
-    /*
-    The following create and insert functions were adaped from the top answer on:
-    https://stackoverflow.com/questions/46738333/using-javascript-loop-to-create-multiple-html-elements
-    */
-
-    // Function to create error messages
-    const createError = (tagName, props) => {
-        return Object.assign( document.createElement(tagName), (props || {}) );
-    }
-
-    // Function to insert error messages
-    const insertError = (targetElement, error, position) => {
-        targetElement.insertAdjacentElement(position, error);
-    }
-
-    //Name
-    const nameError = createError('span', {className: 'error-message is-hidden', textContent: 'Please enter your name.'});
-    insertError( name, nameError, 'afterend');
-
-    // Email
-    const emailError = createError('span', {className: 'error-message is-hidden', textContent: 'Please enter a valid email.'});
-    insertError( email, emailError, 'afterend');
-
-    // Activities
-    const activitiesError = createError('span', {className: 'error-message is-hidden', textContent: 'Please select at least one activity.'});
-    insertError( activities, activitiesError, 'beforeend');
-
-    // CC Number
-    const ccNumError = createError('span', {className: 'error-message is-hidden', textContent: 'Please enter a valid credit card.'});
-    insertError( ccNum, ccNumError, 'afterend');
-
-    // CC Number
-    const zipError = createError('span', {className: 'error-message is-hidden', textContent: 'Please enter zip code.'});
-    insertError( zip, zipError, 'afterend');
-
-    // CVV
-    const cvvError = createError('span', {className: 'error-message is-hidden', textContent: 'Please enter CVV number.'});
-    insertError( cvv, cvvError, 'afterend');
-
 
 
 
@@ -95,9 +81,24 @@ document.addEventListener('DOMContentLoaded', function() {
     // Handlers
     /*****************************************************/
 
-    // Show other job field
+
+    /* Function to create error messages. Arguments are the created tag name and an object for its property key/values
+    I modified code from: https://stackoverflow.com/questions/46738333/using-javascript-loop-to-create-multiple-html-elements */
+    function createError(tagName, props) {
+        return Object.assign( document.createElement(tagName), (props || {}) );
+    }
+
+
+    /* Function to insert error messages. It takes the target element, error message to insert, and position.
+    I modified code from: https://stackoverflow.com/questions/46738333/using-javascript-loop-to-create-multiple-html-elements */
+    function insertError(targetElement, error, position) {
+        targetElement.insertAdjacentElement(position, error);
+    }
+
+
+    /* Handler for the Job Role select.
+    It shows the 'Your Job Role' input when Job Role === other. */
     const showOtherJob = () => {
-        const jobOther = document.getElementById('title-other-container');
         if (jobSelect.value === 'other') {
             jobOther.style.display = 'block';
         } else {
@@ -106,9 +107,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     
 
-   // Handler for Design select
+   /* Handler for the shirt design select input.
+   It controls what options display in the shirt color select based on value of the design select*/
    const showShirtColor = () => {
+
+        // Get the options of the select 
         const colorOptions = document.getElementById('color').options;
+
+        // Define colors for each shirt type
         const punShirtColors = ['cornflowerblue', 'darkslategrey', 'gold'];
         const heartShirtColors = ['tomato', 'steelblue', 'dimgrey'];
 
@@ -116,14 +122,18 @@ document.addEventListener('DOMContentLoaded', function() {
         const punOptions = [];
         const heartOptions = [];
 
-        /* Loop over shirt options. Set all options to hidden.
-           Push pun and heart shirts into the new arrays.*/
+        // Loop over the select options
         for (let i = 0; i < colorOptions.length; i++) {
+
+            // Set all options to hidden.
             colorOptions[i].hidden = true;
+
+            // Bindings for value of shirts and tests to see if values are included in pun or heart shirt arrays
             const colorOptionValue = colorOptions[i].value;
             const isPunShirtColor = punShirtColors.includes(colorOptionValue);
             const isHeartShirtColor = heartShirtColors.includes(colorOptionValue);
 
+            // Push the shirts to the appropriate new array
             if (isPunShirtColor) {
                 punOptions.push(colorOptions[i]);
             } else if (isHeartShirtColor) {
@@ -146,19 +156,25 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
-    // Activity checkbox handler
+    /* Handler for the activity checkboxes - It disables/enables checkboxes based on scheduling conflicts.
+    It also calculates the $ total for the selected activities. */
     const clickActivity = () => {
         
+        // Get target of the click and its day/time and cost data
         const clicked = event.target;
         const clickedDayAndTime = clicked.getAttribute('data-day-and-time');
         const clickedCost = clicked.getAttribute('data-cost');
         
+        // If it's an input clicked (handler will be placed on the .activities div)
         if ( clicked.tagName === 'INPUT') {
     
             for (let i = 0; i < checkboxes.length; i++) {
+
+                // Get day and time and label
                 const checkboxDayAndTime = checkboxes[i].getAttribute('data-day-and-time');
                 const parentLabel = checkboxes[i].parentNode;
-    
+                
+                // Disable/enable checkboxes
                 if ( clickedDayAndTime === checkboxDayAndTime && clicked !== checkboxes[i]) {
                     if (clicked.checked) {
                         checkboxes[i].disabled = true;
@@ -170,7 +186,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             }
 
-            // Calculate running total
+            // Calculate the running total
             if (clicked.checked === true) {
                 totalCost += parseInt(clickedCost);
             } else {
@@ -188,17 +204,16 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
-    // Payment method handler
+    /* Handler for the payment method select input. 
+    It hides/shows content based on the select value */
     const changePayment = () => {
 
         let paymentDivs = [creditCard, paypal, bitcoin];
 
-        // Hide all payment information divs
         paymentDivs.forEach (paymentDiv => {
             paymentDiv.style.display = 'none';
         });
         
-        // Show the appropriate payment info div to match the select value
         if (event.target.value === 'credit card') {
             paymentDivs[0].style.display = 'block';
         } else if (event.target.value === 'paypal') {
@@ -214,13 +229,18 @@ document.addEventListener('DOMContentLoaded', function() {
     // Validation Handlers
     /*****************************************************/
 
-    
+
+    // Function to pass into the validation handlers. It hides input message and removes error class.
+    // Arguments are the target input and input errors
     const hideInputError = (input, inputError) => {
         input.classList.remove('has-error');
         inputError.classList.add('is-hidden');
         return true;
     }
     
+
+    // Function to pass into the validation handlers. It shows input message and adds error class.
+    // Arguments are the target input and input errors
     const showInputError = (input, inputError) => {
         input.classList.add('has-error');
         inputError.classList.remove('is-hidden');
@@ -228,6 +248,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
+    // Check name to see if it isn't empty
     const validateName = () => {
         const nameValue = name.value;
         if (nameValue.length > 0) {
@@ -237,6 +258,8 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+
+    // Check email to see if @ and . aren't first and last 
     const validateEmail = () => {
         const emailValue = email.value;
         const emailAtIndex = emailValue.indexOf('@');
@@ -248,6 +271,8 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+
+    // Check to see at least one checkbox is checked
     const validateActivities = () => {
         for (let i = 0; i < checkboxes.length; i++) {
             if (checkboxes[i].checked) {
@@ -258,6 +283,8 @@ document.addEventListener('DOMContentLoaded', function() {
         showInputError(activities, activitiesError);   
     }
 
+
+    // If paying with credit card, check that CC #, Zip, and CCV meet valid lengths
     const validatePayment = () => {
         const paymentValue = payment.value;
 
@@ -267,6 +294,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const zipValue = zip.value;
             const cvvValue = cvv.value;
 
+            // 16 char max for ccNumb. 13-16 char === valid
             if (ccNumValue.length > 12) {
                 hideInputError(ccNum, ccNumError);
             } else {
@@ -289,18 +317,18 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
-
     /*****************************************************/
-    // Initialize functions
+    // Initialize startup function
     /*****************************************************/
-    inputDefaults(name, payment);
-    showOtherJob();
 
+
+    init();
 
 
     /*****************************************************/
     // Callback functions
     /*****************************************************/
+
 
     jobSelect.addEventListener( 'change', () => {
         showOtherJob();     
@@ -321,13 +349,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     form.addEventListener( 'submit', (e) => {
         e.preventDefault();
-
         validateName();
         validateEmail();
         validateActivities();
         validatePayment();
     });
-
-
-
 });
